419f7d43403fc36969b119136f78fc5d2748e112
Neel Thomas <20cs095@charusat.edu.in>
2023-08-05 02:33:22 +0530

    changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,44 +1,3 @@
-// import React from "react";
-// import './Navbar.css'
-// import 'bootstrap/dist/css/bootstrap.min.css';
-
-// const Navbar = () => {
-//   return (
-//     <div className="navbar">
-//       <ul class="nav nav-pills navbar-dark bg-dark">
-//         <li class="nav-item">
-//           <a class="nav-link" aria-current="page" href="#overview">
-//             Overview
-//           </a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#rooms">
-//             Rooms
-//           </a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#location">
-//             Location
-//           </a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#amenities">
-//             Amenities
-//           </a>
-//         </li>
-//         <li class="nav-item">
-//           <a class="nav-link" href="#policies">
-//             Policies
-//           </a>
-//         </li>
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
-
-
 import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
@@ -78,40 +37,40 @@ const Navbar = () => {
 
   return (
     <div id='header' className='header'>
-    <nav className="navbar navbar-expand-lg fixed-top">
-      <div className="container">
-        <a className="navbar-brand" href="#home">
-          Logo
-        </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#overview" onClick={handleNavLinkClick}>Overview</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#rooms" onClick={handleNavLinkClick}>Rooms</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#location" onClick={handleNavLinkClick}>Location</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#amenities" onClick={handleNavLinkClick}>Amenities</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#policies" onClick={handleNavLinkClick}>Policies</a>
-            </li>
-          </ul>
+      <nav className="navbar navbar-expand-lg fixed-top">
+        <div className="container">
+          <a className="navbar-brand" href="#home">
+            Logo
+          </a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="#overview" onClick={handleNavLinkClick}>Overview</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#rooms" onClick={handleNavLinkClick}>Rooms</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#location" onClick={handleNavLinkClick}>Location</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#amenities" onClick={handleNavLinkClick}>Amenities</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#policies" onClick={handleNavLinkClick}>Policies</a>
+              </li>
+            </ul>
+          </div>
         </div>
+      </nav>
+      <div className="image-container">
+        <img src='images\outside.jpg' alt='room'></img>
+        <div className="image-text"><span>SeaRay</span> Motel</div>
       </div>
-    </nav>
-    <div className="image-container">
-    <img src='images\outside.jpg' alt='room'></img>
-    <div className="image-text"><span>SeaRay</span> Motel</div>
-    </div>
     </div>
   );
 };
